1525d8418112eb72686d1dd6af9a70403a4290f4
Cody-Wise <cody@sharptackmedia.com>
2022-05-17 17:52:38 -0700

    Setup initial files

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,6 @@
+export default async function getPokemon(query) {
+  const rawResponse = await fetch(`/.netlify/functions/Pokemon?pokemon=${query}`);
+  const data = await rawResponse.json();
+
+  return data;
+}
